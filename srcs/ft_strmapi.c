a99a151eff6edbcd7e74dcdd15959342bf39f67f
Giovani Aguirre <gaguirre@e1z2r8p25.42.us.org>
2017-06-25 15:57:59 -0700

    Added one more byte and changed the logic to stop from reading past the string

--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gaguirre <gaguirre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/25 13:23:55 by gaguirre          #+#    #+#             */
-/*   Updated: 2017/06/25 13:24:41 by gaguirre         ###   ########.fr       */
+/*   Updated: 2017/06/25 15:50:52 by gaguirre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 
 	i = 0;
-	if (!s || !(str = (char *)malloc(ft_strlen(s))))
+	if (!s || !(str = (char *)malloc(ft_strlen(s) + 1)))
 		return (0);
 	while (s[i])
 	{
 		str[i] = f(i, s[i]);
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: gaguirre <gaguirre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/25 13:28:35 by gaguirre          #+#    #+#             */
-/*   Updated: 2017/06/25 14:02:53 by gaguirre         ###   ########.fr       */
+/*   Updated: 2017/06/25 15:55:57 by gaguirre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	while (s1[i] || s2[i] || n)
+	while (*s1 && *s2 && n)
 	{
 		if (s1[i] != s2[i])
 			return (0);
