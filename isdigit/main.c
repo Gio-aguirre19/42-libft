f78ee007bb03ba174939a1c038fa218586627c53
Giovani Aguirre <gaguirre@e1z2r8p25.42.us.org>
2017-06-25 15:31:08 -0700

    Cleaning old code

--- isdigit/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <ctype.h>
-int ft_isdigit(unsigned char x);
-
-int main(void){
-	printf("Homemade: %d Library: %d \n", ft_isdigit('9'), isdigit('9'));
-	printf("Homemade: %d Library: %d \n", ft_isdigit('3'), isdigit('3'));
-	printf("Homemade: %d Library: %d \n", ft_isdigit('f'), isdigit('f'));
-}
